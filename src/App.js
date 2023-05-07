44b42202fe50764dd4277955cfd99d1c7fb65140
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-05-07 17:35:33 +0100

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import  styles from './styles'
+import { Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,} from './components'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-black overflow-hidden  w-full'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+      <div className={`bg-black ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+           <Hero/>
+        </div>
+      </div>
+      <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+      
+        <Stats/>
+        < Business/>
+        < Billing/>
+        < CardDeal/>
+        <Testimonials/>
+       < Clients/>
+         <CTA/>
+        <Footer/>
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
